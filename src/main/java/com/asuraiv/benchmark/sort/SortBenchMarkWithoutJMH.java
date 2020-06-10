a5a17698b01a42c3b7cefe9702cf1ba42ceb866b
asuraiv <asuraiv7@gmail.com>
2020-06-10 23:07:41 +0900

    JMH 미사용 벤치마크 코드 추가 (비교 분석을 위해)

---
+++ src/main/java/com/asuraiv/benchmark/sort/SortBenchMarkWithoutJMH.java
@@ -0,0 +1,43 @@
+package com.asuraiv.benchmark.sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/*
+	java option:
+	-Xms2048m -Xmx2048m -XX:+PrintCompilation -verbose:gc
+ */
+public class SortBenchMarkWithoutJMH {
+
+	private static final int N = 1_000;
+	private static final int I = 150_000;
+
+	private static List<Integer> testData = new ArrayList<>();
+
+	static {
+
+		Random random = new Random();
+
+		for(int i = 0; i < N; i++) {
+			testData.add(random.nextInt(Integer.MAX_VALUE));
+		}
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println("정렬 알고리즘 테스트");
+
+		double startTime = System.nanoTime();
+
+		for (int i = 0; i < I; i++) {
+			List<Integer> copiedData = new ArrayList<>(testData);
+			Collections.sort(copiedData);
+		}
+
+		double endTime = System.nanoTime();
+		double timePerOperation = ((endTime - startTime) / (1_000_000_000L * I));
+		System.out.println("결과: " + (1 / timePerOperation) + " op/s");
+	}
+}
